5d2f12c842e34827b317a818593e9592c160b652
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2023-04-06 14:30:06 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -2,7 +2,7 @@
 
 from pyrogram import __version__
 from bot import Bot
-from config import OWNER_ID, CADMIN1, CADMIN2
+from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 @Bot.on_callback_query()
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> ○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n ○ Admin 1 : <a href='tg://user?id={CADMIN1}'>View Profile</a>\n ○ Admin 2 : <a href='tg://user?id={CADMIN2}'>Admin 2</a>\n ○ Language : <code>Python3 & Pyrogram</code>\n ○ Channel : @Otaku_Library\n ○ Chat Group : @Otaku_Chats</b>",
+            text = f"<b> ○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n ○ Admin 1 : <a href='tg://user?id={1787248836}'>View Profile</a>\n ○ Admin 2 : <a href='tg://user?id={1119995812}'>Admin 2</a>\n ○ Language : <code>Python3 & Pyrogram</code>\n ○ Channel : @Otaku_Library\n ○ Chat Group : @Otaku_Chats</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
