b00490dbde1503135501bb18965734bebf163763
Otakubhai <87870903+Otakubhai@users.noreply.github.com>
2023-04-09 18:40:59 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b> ○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n ○ Admin 1 : <a href='tg://user?id=1787248836'>View Profile</a>\n ○ Admin 2 : <a href='tg://user?id=1119995812'>Admin 2</a>\n ○ Language : <code>Python3 & Pyrogram</code>\n ○ Channel : @Otaku_Library\n ○ Chat Group : @Otaku_Chats</b>",
+            text = f"<b> ○ Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n ○ Admin 1 : <a href='tg://user?id=1787248836'>View Profile</a>\n ○ Admin 2 : <a href='tg://user?id=1119995812'>View Profile</a>\n ○ Language : <code>Python3 & Pyrogram</code>\n ○ Channel : @Otaku_Library\n ○ Chat Group : @Otaku_Chats</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
