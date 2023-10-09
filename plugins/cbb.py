174101b9ba4d18e8bae2ad2c4e2b692a043823cd
ANIMExABYSS <146233831+Otakubro1@users.noreply.github.com>
2023-10-09 10:59:56 +0530

    Update cbb.py
    test'

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○🎃 Creator : <a href='tg://user?id={OWNER_ID}'>This Person</a>\n○ 🤗 Developed by  : @Ktgp_3453\n○ 😶‍🌫️ Source Code : <a href='https://r.mtdv.me/github-direct-telebot-file'>Click here</a>\n○ ❤️ Anime Channel : @AnimeXabyss\n○😎 Ongoing Channel : <a href='https://t.me/Ongoing_Anime_Abyss'>AnimeXAbyss</a>\n○ 🤗 Admin 1 : @Ktgp_3453</b>" ,
+            text = f"<b>○ 🎃 Owner : @The_Strawhat_Luffy\n○ ❤️ Anime Channel : @AnimeXabyss\n○ 😎 Ongoing Channel : <a href='https://t.me/Ongoing_Anime_Abyss'></a>\n○ 🤗 Developed by  : @Ktgp_3453\n○ 😶‍🌫️ Source Code : <a href='https://r.mtdv.me/github-direct-telebot-file'>Click here</a></b>" ,
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
